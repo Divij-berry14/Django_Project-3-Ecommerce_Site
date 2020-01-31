6b48adfd3be9a3fa4b104393a7bcb68661f6195d
Divij <divij.berry1999@gmail.com>
2020-01-31 11:31:06 +0530

    Adding mulitple slides To be continued

--- Ecommerce_website/shop/views.py
+++ Ecommerce_website/shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 def index(request):
-    return render(request,'shop/index.html')
+    products=Product.objects.all()
+    print(products)
+    n=len(products)
+    nSlides=n//4 +ceil((n/4) -(n//4))
+    param={'No. of Slides':nSlides,'range':range(nSlides),'product':products}
+    return render(request,'shop/index.html',param)
 
 def about(request):
     return render(request,'shop/about.html')
